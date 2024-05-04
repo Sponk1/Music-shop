82e36c4fcc123ea0eeeab1c05c1b75933e59f728
sponk <99675158+Sponk1@users.noreply.github.com>
2024-05-04 15:56:37 +0300

    search bar done

---
+++ strunka/src/components/PageComponents/ForHomePage/HomeBody.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import "./homeBody.css"
+
+
+const HomeBody = () => {
+    return(
+        <div>
+            <div className="full-body">
+                <div className="search-bar">
+                    <input className="search-input" type="text" placeholder="поиск"/>
+                </div>
+                <div className="tools-img">
+
+                </div>
+                <div className="map-n-desc">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomeBody;
--- strunka/src/pages/Home.jsx
+++ strunka/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "../components/Header/Header"
+import HomeBody from "../components/PageComponents/ForHomePage/HomeBody";
 
 function Home() {
     return(
         <div>
             <Header />
+            <HomeBody />
         </div>
     );
 }
