60a90815b0c7d10dbab0bab67b4812d681bca104
sponk <99675158+Sponk1@users.noreply.github.com>
2024-05-29 23:31:09 +0300

    р

--- strunka/src/App.jsx
+++ strunka/src/App.jsx
@@ -4,9 +4,12 @@ import Home from './pages/Home'
 import CatalogMain from './pages/CatalogMain'
 import Clients from './pages/Clients'
 import AboutCompany from './pages/AboutCompany'
+import Basket from './pages/Basket'
 
 
 function App() {
+
+  let isAuth = false;
 
   return (
     <div className="wrapper">
@@ -17,6 +20,10 @@ function App() {
           <Route path='/CatalogMain' element={<CatalogMain />} />
           <Route path='/Clients' element={<Clients />} />
           <Route path='/AboutCompany' element={<AboutCompany />} />
+          <Route path='*' element={<Home />} />
+          {
+            isAuth && <Route path='/Basket' element={<Basket />} />
+          }
         </Routes>
       </BrowserRouter>
     </div>
